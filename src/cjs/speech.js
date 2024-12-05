e5510fefc87b02b840a0a48655a24f0d27e986aa
kaikato <kaiykato@gmail.com>
2024-12-05 01:00:03 +0000

    Add model parameter to speech methods

--- src/cjs/speech.js
+++ src/cjs/speech.js
@@ -313,7 +313,7 @@ class Speech {
     const formData = new _formData.default();
     formData.append('text', text);
     formData.append('voice', voice);
-    const fields = ['format', 'language', 'conversational', 'length', 'return_durations', 'return_seed', 'sample_rate', 'seed', 'speed'];
+    const fields = ['format', 'language', 'model', 'conversational', 'length', 'return_durations', 'return_seed', 'sample_rate', 'seed', 'speed'];
     fields.forEach(field => {
       if (field in options) {
         if (typeof options[field] === 'boolean') {
