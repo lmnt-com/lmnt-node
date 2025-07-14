40b5848a4deb27d951362fcad804258faa21c7e0
kaikato <kaiykato@gmail.com>
2025-07-14 23:28:28 +0000

    Update create voice example

--- examples/create_voice.ts
+++ examples/create_voice.ts
@@ -7,16 +7,12 @@ async function main() {
   const lmnt = new Lmnt();
 
   // Create a demo voice. Once completed, it will be available in your lsit of voices.
-  const metadata = {
-    name: 'demo-voice',
-    enhance: false,
-    type: 'instant',
-    gender: 'female',
-    description: 'This is a demo voice.',
-  };
   const createVoiceResponse = await lmnt.voices.create({
     files: [fs.createReadStream(path.join(__dirname, 'sample-audio.mp3'))],
-    metadata: JSON.stringify(metadata),
+    name: 'demo-voice',
+    enhance: false,
+    gender: 'female',
+    description: 'This is a demo voice.',
   });
   console.log('Results from `createVoice`:', createVoiceResponse);
 }
